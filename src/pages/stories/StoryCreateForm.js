3b034e4a5ec3d6905f6da5e227da017dc0cd2e12
Sara Smith <saraa.smith@hotmail.com>
2023-08-11 14:07:29 +0000

    Add handlers to create story form for changing of image and submit, and add errror messages

--- src/pages/stories/StoryCreateForm.js
+++ src/pages/stories/StoryCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -12,6 +12,9 @@ import styles from "../../styles/StoryCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
+import { Alert, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function StoryCreateForm() {
 
@@ -24,6 +27,9 @@ function StoryCreateForm() {
         image: "",
     });
         const { title, destination, content, image } = postData;
+
+    const imageInput = useRef(null);
+    const history = useHistory();
     
     const handleChange = (event) => {
         setPostData({
@@ -42,6 +48,25 @@ function StoryCreateForm() {
         }
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+    
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
+    
+        try {
+          const { data } = await axiosReq.post("/posts/", formData);
+          history.push(`/stories/${data.id}`);
+        } catch (err) {
+          console.log(err);
+          if (err.response?.status !== 401) {
+            setErrors(err.response?.data);
+          }
+        }
+      };
+
     const textFields = (
         <div className="text-center">
             <Form.Group>
@@ -53,6 +78,11 @@ function StoryCreateForm() {
                 onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.title?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
             <Form.Group>
                 <Form.Label>Destination</Form.Label>
                 <Form.Control
@@ -62,16 +92,26 @@ function StoryCreateForm() {
                 onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.destination?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
             <Form.Group>
                 <Form.Label>Content</Form.Label>
                 <Form.Control
-                type="textarea"
+                as="textarea"
                 rows={10}
                 name="content"
                 value={content}
                 onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.content?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
             <Button
                 className={`${btnStyles.Button} ${btnStyles.Cancel}`}
                 onClick={() => {}}>
@@ -84,25 +124,46 @@ function StoryCreateForm() {
     );
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container
                         className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
                         <Form.Group className="text-center">
+                        {image ? (
+                            <>
+                                <figure>
+                                    <Image className={appStyles.Image} src={image} rounded />
+                                </figure>
+                                <div>
+                                    <Form.Label
+                                    className={`${btnStyles.Button} ${btnStyles.Orange} btn`}
+                                    htmlFor="image-upload">
+                                    Change image
+                                    </Form.Label>
+                                </div>
+                            </>
+                        ) : (
                             <Form.Label
-                            className="d-flex justify-content-center"
-                            htmlFor="image-upload">
+                                className="d-flex justify-content-center"
+                                htmlFor="image-upload">
                             <Asset
                                 src={defaultImage}
                                 message="Click the camera to upload an image"/>
                             </Form.Label>
-                            <Form.File
-                                id="image-upload"
-                                accept="image/*"
-                                onChange={handleChangeImage}
-                                ref={imageInput}/>
+                        )}
+                        <Form.File
+                            id="image-upload"
+                            accept="image/*"
+                            onChange={handleChangeImage}
+                            ref={imageInput}
+                            />
                         </Form.Group>
+                        {errors?.image?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
                         <div className="d-none">{textFields}</div>
                     </Container>
                 </Col>
