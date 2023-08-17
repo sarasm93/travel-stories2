9048dead8f6fdf74cc17cd0982346ef8e7d97763
Sara Smith <saraa.smith@hotmail.com>
2023-08-17 11:41:13 +0000

    Add destinationpage.js with useeffect hook to fetch and mount single destination item

---
+++ src/pages/destinations/DestinationPage.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Destination from "./Destination";
+
+function DestinationPage() {
+    const { id } = useParams();
+    const [destination, setDestination] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: destination }] = await Promise.all([
+                axiosReq.get(`/destinations/${id}`),
+                ]);
+                setDestination({ results: [destination] });
+                console.log(destination)
+            } catch (err) {
+                console.log(err);
+            }
+            };
+
+        handleMount();
+        }, [id]);
+
+    return (
+        <Row>
+            <Col>
+                <Destination {...destination.results[0]} setDestinations={setDestination} />
+            </Col>
+        </Row>
+    );
+}
+
+export default DestinationPage;
