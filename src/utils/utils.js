19f8e10820b1c8ed3f496bdcdae3970b2273c90a
Sara Smith <saraa.smith@hotmail.com>
2023-08-25 07:20:29 +0000

    Add functions to utils.js to clear console of errors caused by failed token refresh requests when logged out

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -15,3 +16,15 @@ export const fetchMoreData = async (resource, setResource) => {
   } catch (err) {}
 };
 
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestamp");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refreshTokenTimestamp");
+};
